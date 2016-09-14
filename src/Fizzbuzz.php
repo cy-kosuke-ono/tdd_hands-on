b221ddc6f2c450d060262843f5c1d4c71f7d6e29
cy-kosuke-ono <kosuke_ono@cybird.co.jp>
2016-09-14 19:01:42 +0900

    FizzBuzzのとりあえずの実装

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -8,6 +8,20 @@ class Fizzbuzz
     }
 
     public function call($value){
+      
+      if ($value % 15 === 0){
+        return "FizzBuzz";
+      }
+
+      if ($value % 3 === 0){
+        return "Fizz";
+      }
+      if ($value % 5 ===0){
+        return "Buzz";
+      }
+
       return $value;
     }
+
+
 }
--- test/FizzbuzzTest.php
+++ test/FizzbuzzTest.php
@@ -11,4 +11,31 @@ class FizzbuzzTest extends \PHPUnit_Framework_TestCase
     $this->assertSame(1, $fizzbuzz->call($expected));
   }
 
+  /**
+   * @test
+   */
+  public function 引数が3の倍数のときFizzを返す(){
+    $fizzbuzz = new FizzBuzz();
+    $expected = 6;
+    $this->assertSame("Fizz", $fizzbuzz->call($expected));
+  }
+
+  /**
+   * @test
+   */
+  public function 引数が5の倍数のときBuzzを返す(){
+    $fizzbuzz = new FizzBuzz();
+    $expected = 25;
+    $this->assertSame("Buzz", $fizzbuzz->call($expected));
+  }
+
+  /**
+   * @test
+   */
+  public function 引数が15の倍数のとき最優先でFizzBuzzを返す(){
+    $fizzbuzz = new FizzBuzz();
+    $expected = 45;
+    $this->assertSame("FizzBuzz", $fizzbuzz->call($expected));
+  }
+
 }
